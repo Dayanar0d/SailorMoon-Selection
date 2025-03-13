793e6d3d3f6caa261f756b9960048c144b5f3c4e
Dayana <Ddiaz413@west-mec.oeg>
2025-03-13 14:27:03 -0700

    failed animation tests :(

--- java.js
+++ java.js
@@ -3,9 +3,11 @@ const Mercury = document.getElementsByClassName("item2");
 const Mars = document.getElementByClass("item3");
 const Jupiter = document.getElementByClass("item4");
 const Venus = document.getElementByClass("item5");
-const container = document.getElementByClass("flex-container");
 
-function selectChar() {
-    Mercury.style.animation = "mymove 4s ";
-    Mercury.style.animationFillMode = "forwards";
-}
+// Get the div by its ID
+// const div1 = document.getElementById("myDIV");
+
+// function myFunction() {
+//     // Apply the animation property correctly
+//     div1.style.animation = "mymove 4s forwards"; // Trigger the animation
+// }
