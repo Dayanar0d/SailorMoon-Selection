00eb83ab6b62e9e8d98788c0dfb21a941f27f80d
Dayana <Ddiaz413@west-mec.oeg>
2025-03-14 13:57:03 -0700

    last changes in class

--- java.js
+++ java.js
@@ -1,13 +1,38 @@
-const Moon = document.getElementByClass("item1");
-const Mercury = document.getElementsByClassName("item2");
-const Mars = document.getElementByClass("item3");
-const Jupiter = document.getElementByClass("item4");
-const Venus = document.getElementByClass("item5");
+const Moon = document.getElementById('sailor-moon');
+const Mercury = document.getElementById('sailor-mercury');
+const Mars = document.getElementById('sailor-mars');
+const Jupiter = document.getElementById('sailor-jupiter');
+const Venus = document.getElementById('sailor-venus');
+const Guardians = document.getElementsByClassName('flex-item');
+const charName = document.getElementById('char-name');
+const charScreen = document.getElementById('character-screen');
+
 
 // Get the div by its ID
 // const div1 = document.getElementById("myDIV");
 
+
 // function myFunction() {
-//     // Apply the animation property correctly
-//     div1.style.animation = "mymove 4s forwards"; // Trigger the animation
+//     Guardians.addEventListener('click', function(event) {
+//         // div1.style.animation = "mymove 4s forwards";
+//         charSum.style.color = white;
+//     });
+// };
+
+
+// function myFunction() {
+//     charInfo.textContent = "this works!!!";
+//     charInfo.style.color = "white";
 // }
+
+
+Array.from(Guardians).forEach(guardian => {
+    guardian.addEventListener('click', function () {
+        // Change the color of the text when clicking on a character
+        charName.style.opacity = 1; // Set the color to white
+        // charScreen.style.display = !;
+        document.getElementById("character-screen").style.display = "block";
+    });
+});
+
+
